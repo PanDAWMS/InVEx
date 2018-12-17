d782a2d7c296ba1701b1609045fd9663c4e45ff3
Mikhail Titov <mikhail.a.titov@gmail.com>
2018-12-17 19:12:49 +0300

    updated manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+
 import os
 import sys
-from os.path import abspath, dirname, split
 
-if __name__ == '__main__':
-    #DJANGO_SETTINGS_MODULE = '%s.%s' % (split(abspath(dirname(__file__)))[1], 'settings')
-    DJANGO_SETTINGS_MODULE = '%s' % ('core.settings')
+DJANGO_SETTINGS_MODULE = '%s'.format('core.settings')
+
+
+def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', DJANGO_SETTINGS_MODULE)
     try:
         from django.core.management import execute_from_command_line
@@ -16,3 +17,7 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
