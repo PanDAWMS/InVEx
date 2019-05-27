7ef69fcb1f25cb7adcdb890bf3d020127ae3d0e7
Dmitry Grin <dymong@yandex.ru>
2019-05-27 12:30:10 +0300

    Moved new datasets to private folder and removed tests due to being non-deterministic

--- core/calc/tests/test.py
+++ core/calc/tests/test.py
@@ -1,15 +1,13 @@
 # import from csv file test
-# from calc.tests import importcsv_test
+from calc.tests import importcsv_test
 from calc.tests import basicstatistics_test
 from calc.tests import operationexample_test
 from calc.tests import operationhistory_test
 from calc.tests import KMeansClustering_test
-from calc.tests import MiniBatchKMeansClustering_test
 
 
-# importcsv_test.run()
+importcsv_test.run()
 basicstatistics_test.run()
 operationexample_test.run()
 operationhistory_test.run()
 KMeansClustering_test.run()
-MiniBatchKMeansClustering_test.run()
