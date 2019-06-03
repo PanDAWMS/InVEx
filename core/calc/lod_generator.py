dd49e9d03e5e2a6f808ab902b64c6ce004c9d93a
maria <magsend@gmail.com>
2019-06-03 14:19:55 +0300

    fix: group_features was added to LoD Metadata
    group_features represents the title for the group

--- core/calc/lod_generator.py
+++ core/calc/lod_generator.py
@@ -59,19 +59,20 @@ class LoDGenerator:
             group_labels = self._get_labels_kmeans_clustering(
                 n_clusters=num_groups,
                 features=features)
-            self.group_name = 'group'
-            self.dataset[self.group_name] = group_labels
-            self.dataset.set_index(self.group_name)
+            self.group_features = 'cluster'
+            self.dataset[self.group_features] = group_labels
+            self.dataset.set_index(self.group_features)
             self.grouped_dataset = self._get_groups_mean()
             self._update_groups_metadata()
         elif mode == 'param_categorical':
-            self.group_name = features
+            self.group_features = features
             self.grouped_dataset = self._get_groups_mean()
             self._update_groups_metadata()
         elif mode == 'param_num_continuous':
             curr_variable = self.dataset[features[0]].max()
             intervals = self.set_intervals(curr_variable)
             group_number = -1
+            self.group_features = str(features[0]) + '_intervals'
 
             if self._groups_metadata:
                 del self._groups_metadata[:]
@@ -82,17 +83,16 @@ class LoDGenerator:
                 group_number += 1
                 group = self.dataset[((self.dataset[features[0]] >= i[0]) & (self.dataset[features[0]] < i[1]))]
                 group_meta = {}
-                self.group_name = str(round(i[0], 2)) + '-' + str(round(i[1], 2))
-                group_meta['group_name'] = self.group_name
+                group_meta['group_name'] = str(round(i[0], 2)) + '-' + str(round(i[1], 2))
                 group_meta['group_number'] = group_number
                 group_meta['group_indexes'] = group.index.tolist()
                 group_meta['group_length'] = len(group)
                 group_meta['group_koeff'] = log1p(len(group) * 100 / self.num_initial_elements)
                 self._groups_metadata.append(group_meta)
                 group_mean = group.mean()
-                group_mean['group'] = self.group_name
+                group_mean[self.group_features] = group_meta['group_name']
                 self.grouped_dataset = self.grouped_dataset.append(group_mean, ignore_index=True)
-            self.grouped_dataset.set_index('group', inplace=True)
+            self.grouped_dataset.set_index(self.group_features, inplace=True)
         else:
             raise NotImplementedError
 
@@ -106,7 +106,7 @@ class LoDGenerator:
                                **MINIBATCH_PARAMS_DEFAULT).fit_predict(data)
 
     def _get_groups_mean(self):
-        return self.dataset.groupby(self.group_name).mean()
+        return self.dataset.groupby(self.group_features).mean()
 
     def _update_groups_metadata(self):
         """
@@ -124,7 +124,7 @@ class LoDGenerator:
             if self._groups_metadata:
                 del self._groups_metadata[:]
 
-            grouped = self.dataset.groupby(self.group_name)
+            grouped = self.dataset.groupby(self.group_features)
 
             # assotiate groups with ordinal numbers
             group_number = -1
