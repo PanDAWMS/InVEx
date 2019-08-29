Corrected a scaling issue for dendrogram.

core/calc/HierarchicalClustering.py
@@ -64,12 +64,12 @@ class HierarchicalClustering(baseoperationclass.BaseOperationClass):
     def find_linkage(self, dataset):
         distance_metric = 'euclidean'
         categorical_indices = get_categorical_indices(dataset)
+        numerical_indices = [x for x in range(dataset.shape[1]) if x not in categorical_indices]
         if categorical_indices:
             dataset = normalized_dataset(dataset, categorical_indices)
-            dataset = encode_nominal_parameters(dataset)
+            dataset = encode_nominal_parameters(dataset, categorical_indices)
             if self.categorical_weight is None:
-                self.categorical_weight = 0.5 * dataset.values.std()
-                print(self.categorical_weight)
+                self.categorical_weight = 0.5 * dataset.take(numerical_indices, axis=1).values.std()
             from .util.dissimilarity import mixed_metric
             distance_metric = partial(mixed_metric, categorical_indices=np.array(categorical_indices, dtype=np.int32), categorical_weight=self.categorical_weight)
         dataset = dataset.values.astype(np.float64)

core/calc/util/__init__.py
@@ -1,8 +1,9 @@
 import numpy as np
-from sklearn.preprocessing import scale
+from sklearn.preprocessing import maxabs_scale
 
 import pyximport
-pyximport.install(setup_args={"include_dirs": [np.get_include()]})
+pyximport.install(inplace=True, setup_args={"include_dirs": [np.get_include()],
+                                            "directives": {'language_level': 3}})
 
 
 def get_categorical_indices(dataset):
@@ -23,19 +24,23 @@ def _get_encoding_map(values):
     return encoding_map
 
 
-def encode_nominal_parameters(dataset):
+def encode_nominal_parameters(dataset, nominal_indices=()):
+    nominal_columns = []
+    for index, column in enumerate(dataset.columns):
+        if index in nominal_indices:
+            nominal_columns.append(column)
     for column, values in dataset.items():
-        if dataset[column].dtype.name not in ("float64", "float32", "int64", "int32"):
+        if column in nominal_columns or dataset[column].dtype.name not in ("float64", "float32", "int64", "int32"):
             encoding_map = _get_encoding_map(values.values)
             dataset[column] = values.apply(encoding_map.get)
     return dataset
 
 
-def normalized_dataset(dataset, indices_to_ignore):
+def normalized_dataset(dataset, indices_to_ignore=()):
     dataset = dataset.copy()
     for index, column in enumerate(dataset.columns):
         if index in indices_to_ignore or dataset[column].dtype.name not in ("float64", "float32", "int64", "int32"):
             continue
-        values = scale(dataset[column].values.astype(np.float64))
+        values = maxabs_scale(dataset[column].values.astype(np.float64), copy=False)
         dataset[column] = values
     return dataset