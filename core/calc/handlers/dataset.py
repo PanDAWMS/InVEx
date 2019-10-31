cd645d20ad8d13b22ee65871a4f89a569fe4d157
Mikhail Titov <mikhail.a.titov@gmail.com>
2019-10-31 23:15:19 +0300

    update: moved LoD groups and modifications to HDF5 storage

--- core/calc/handlers/dataset.py
+++ core/calc/handlers/dataset.py
@@ -18,7 +18,6 @@ from ...providers import LocalReader
 from ..lod_generator import LoDGenerator
 
 from ._base import BaseDataHandler
-from .groupeddata import GroupedDataHandler
 from .operation import OperationHandler
 
 FILE_EXTENSION_DEFAULT = 'h5'
@@ -89,7 +88,39 @@ class DatasetHandler(BaseDataHandler):
                 self._origin = dataset
 
         elif kwargs.get('load_history_data', False):
-            self._load_data_stack(operation_id=kwargs.get('operation_id'))
+            self._load_from_hdf5(operation_id=kwargs.get('operation_id'))
+
+    def _set_base_data_to_hdf5(self, base_group, df):
+        base_group.attrs['date'] = str(datetime.utcnow())
+
+        # "default" dataset
+        base_group.create_dataset(
+            name='default',
+            data=np.array(df.to_records(
+                index=True,
+                column_dtypes=dict(zip(df.columns.to_list(), map(
+                    lambda x: h5py.special_dtype(vlen=str)
+                    if x in [np.dtype('O'), np.dtype('S'), np.dtype('U')]
+                    else x, df.dtypes.to_list()))),
+                index_dtypes=h5py.special_dtype(vlen=str))))
+        base_group['default'].attrs['index'] = df.index.name
+
+        # "features_description" dataset
+        # TODO: Check - is it necessary to apply "dropNA"?
+        df.dropna(axis=1, how='all', inplace=True)
+        df.dropna(axis=0, how='all', inplace=True)
+        base_group.create_dataset(
+            name='features_description',
+            data=json.dumps(self._get_dataset_features_description(df),
+                            default=json_default_int))
+
+    @staticmethod
+    def _get_modified_group_path(group_ids=None):
+        group_path = '/base/modified'
+        if group_ids:
+            group_path += '/{}/modified'.format(
+                '/modified/'.join(map(lambda x: f'group_{x}', group_ids)))
+        return group_path
 
     def create_dataset_storage(self, df):
         """
@@ -99,32 +130,9 @@ class DatasetHandler(BaseDataHandler):
         :type df: pandas.DataFrame
         """
         with h5py.File(self._file_name, 'w') as f:
-
-            # "base" group
             base_group = f.create_group('base')
             base_group.attrs['user'] = ''
-            base_group.attrs['date'] = str(datetime.utcnow())
-
-            # "default" dataset
-            base_group.create_dataset(
-                name='default',
-                data=np.array(df.to_records(
-                    index=True,
-                    column_dtypes=dict(zip(df.columns.to_list(), map(
-                        lambda x: h5py.special_dtype(vlen=str)
-                        if x in [np.dtype('O'), np.dtype('S'), np.dtype('U')]
-                        else x, df.dtypes.to_list()))),
-                    index_dtypes=h5py.special_dtype(vlen=str))))
-            base_group['default'].attrs['index'] = df.index.name
-
-            # "features_description" dataset
-            # TODO: Check - is it necessary to apply "dropNA"?
-            df.dropna(axis=1, how='all', inplace=True)
-            df.dropna(axis=0, how='all', inplace=True)
-            base_group.create_dataset(
-                name='features_description',
-                data=json.dumps(self._get_dataset_features_description(df),
-                                default=json_default_int))
+            self._set_base_data_to_hdf5(base_group=base_group, df=df)
 
     def _get_initial_dataset(self, **kwargs):
         """
@@ -135,6 +143,10 @@ class DatasetHandler(BaseDataHandler):
         :return: Initial or grouped dataset sample.
         :rtype: pandas.DataFrame/None
         """
+        err_msg_subj = '[DatasetHandler._get_initial_dataset]'
+
+        output = None
+
         if self._group_ids is None:
 
             with h5py.File(self._file_name, 'r') as f:
@@ -143,21 +155,71 @@ class DatasetHandler(BaseDataHandler):
                 output.set_index(f['base']['default'].attrs['index'],
                                  inplace=True)
 
-                if kwargs.get('usecols'):
-                    output = output.loc[:, kwargs['usecols']]
-
                 self.features_description = json.loads(
                     f['base']['features_description'][()])
 
         elif self._group_ids:
-            output = GroupedDataHandler(did=self._did,
-                                        group_ids=self._group_ids[:-1]).\
-                get_group(group_id=int(self._group_ids[-1]))
+
+            group_id = int(self._group_ids[-1])
+            group_name = f'group_{group_id}'
+
+            with h5py.File(self._file_name, 'r+') as f:
+
+                # root "modified" group
+                m_path = self._get_modified_group_path(self._group_ids[:-1])
+                modified_group = f[m_path]
+
+                sub_modified_group = modified_group.get(group_name)
+                if sub_modified_group is not None:
+
+                    output = pd.DataFrame.from_records(
+                        sub_modified_group['default'][()])
+                    output.set_index(
+                        sub_modified_group['default'].attrs['index'],
+                        inplace=True)
+
+                    self.features_description = json.loads(
+                        sub_modified_group['features_description'][()])
+
+                else:
+
+                    lod = json.loads(modified_group['lod'][()]) or {}
+                    if lod.get('groups') is None:
+                        logger.error(f'{err_msg_subj} LoD data is not set')
+                        raise Exception
+
+                    group_indexes = None
+                    for group_data in lod['groups']:
+                        if group_data['group_number'] == group_id:
+                            group_indexes = group_data['group_indexes']
+                            break
+
+                    if group_indexes is None:
+                        logger.error(f'{err_msg_subj} LoD Group is not found')
+                        raise Exception
+
+                    base_group = f[m_path.rsplit('/', 1)[0]]
+                    df = pd.DataFrame.from_records(base_group['default'][()])
+                    df.set_index(base_group['default'].attrs['index'],
+                                 inplace=True)
+
+                    output = df.loc[
+                        group_indexes,
+                        json.loads(modified_group.attrs['selected_features'])]
+
+                    sub_modified_group = modified_group.create_group(group_name)
+                    self._set_base_data_to_hdf5(base_group=sub_modified_group,
+                                                df=output)
+
+                self.features_description = json.loads(
+                    sub_modified_group['features_description'][()])
+
         else:
-            output = None
-            logger.error('[DatasetHandler._get_initial_dataset] '
-                         'Group ids are not set - '
+            logger.error(f'{err_msg_subj} Group ids are not set - '
                          f'did: {self._did}, group_ids: {self._group_ids}')
+
+        if output is not None and kwargs.get('usecols'):
+            output = output.loc[:, kwargs['usecols']]
         return output
 
     @property
@@ -211,12 +273,6 @@ class DatasetHandler(BaseDataHandler):
             self._modifications['auxiliary'] = lod.grouped_dataset.drop(
                 self._origin.columns.tolist(), 1)
             self._property_set['lod']['groups'] = lod.get_groups_metadata()
-
-            GroupedDataHandler(did=self._did, group_ids=self._group_ids).\
-                set_groups(
-                    dataset=dataset.loc[:, self._property_set['features']],
-                    groups_metadata=self._property_set['lod']['groups'],
-                    save_to_file=True)
         else:
             self._origin = local_reader.get_numeric_data(dataset)
             _set = set(self._origin.columns.tolist())
@@ -227,31 +283,35 @@ class DatasetHandler(BaseDataHandler):
 
         self._modifications['normalized'] = local_reader.scaler(self._origin)
 
-    def _save_data_stack(self, reprocess=False):
+    def _save_to_hdf5(self, reprocess=False):
         """
         Save modifications of the initial dataset and corresponding data.
 
         :param reprocess: Flag to force re-creation of dataset modifications.
         :type reprocess: bool
         """
-        err_msg_subj = '[DatasetHandler._save_data_stack]'
+        err_msg_subj = '[DatasetHandler._save_to_hdf5]'
 
         if not os.path.isfile(self._file_name):
             logger.error(f'{err_msg_subj} Failed to find the file '
                          f'({self._file_name})')
 
+        m_path = self._get_modified_group_path(self._group_ids)
+
         if reprocess:
             with h5py.File(self._file_name, 'a') as f:
-                if f['base'].get('modified') is not None:
-                    del f['base']['modified']
+                if f.get(m_path) is not None:
+                    del f[m_path]
 
         try:
             with h5py.File(self._file_name, 'r+') as f:
 
                 # "modified" group
-                modified_group = f['base'].get('modified')
+                modified_group = f.get(m_path)
                 if modified_group is None:
-                    modified_group = f['base'].create_group('modified')
+
+                    base_path = m_path.rsplit('/', 1)[0]
+                    modified_group = f[base_path].create_group('modified')
                     modified_group.attrs['date'] = str(datetime.utcnow())
                     modified_group.attrs['selected_features'] = json.dumps(
                         self._property_set['features'])
@@ -295,30 +355,34 @@ class DatasetHandler(BaseDataHandler):
                          f'({self._file_name}): {e}')
             raise
 
-    def _load_data_stack(self, **kwargs):
+    def _load_from_hdf5(self, **kwargs):
         """
         Load dataset modifications and corresponding data from the storage file.
 
         :keyword operation_id: Operation id to be loaded.
         """
-        err_msg_subj = '[DatasetHandler._load_data_stack]'
+        err_msg_subj = '[DatasetHandler._load_from_hdf5]'
 
         if not os.path.isfile(self._file_name):
-            logger.error(f'{err_msg_subj} Failed to find the file '
+            logger.error(f'{err_msg_subj} Failed to find the storage file '
                          f'({self._file_name})')
 
         try:
             with h5py.File(self._file_name, 'r') as f:
 
-                self.features_description = json.loads(
-                    f['base']['features_description'][()])
+                modified_group_path = \
+                    self._get_modified_group_path(self._group_ids)
 
-                modified_group = f['base']['modified']
+                base_group = f[modified_group_path.rsplit('/', 1)[0]]
+                modified_group = f[modified_group_path]
+
+                self.features_description = json.loads(
+                    base_group['features_description'][()])
 
                 self._origin = pd.DataFrame.from_records(
                     modified_group['numeric_real'][()])
-                self._origin.set_index(f['base']['default'].attrs['index'],
-                                       inplace=True)
+                self._origin.set_index(
+                    base_group['default'].attrs['index'], inplace=True)
 
                 self._modifications.update({
                     'normalized': pd.DataFrame.from_records(
@@ -326,10 +390,10 @@ class DatasetHandler(BaseDataHandler):
                     'auxiliary': pd.DataFrame.from_records(
                         modified_group['auxiliary'][()])})
 
-                self._normalized.set_index(f['base']['default'].attrs['index'],
-                                           inplace=True)
-                self._auxiliary.set_index(f['base']['default'].attrs['index'],
-                                          inplace=True)
+                self._normalized.set_index(
+                    base_group['default'].attrs['index'], inplace=True)
+                self._auxiliary.set_index(
+                    base_group['default'].attrs['index'], inplace=True)
 
                 self._property_set.update({
                     'features': json.loads(
@@ -448,4 +512,4 @@ class DatasetHandler(BaseDataHandler):
         """
         if (self._origin is not None and
                 self._modifications and self._property_set):
-            self._save_data_stack(reprocess=reprocess)
+            self._save_to_hdf5(reprocess=reprocess)
